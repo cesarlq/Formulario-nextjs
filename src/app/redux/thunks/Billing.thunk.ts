835860520cac38f70f887e045579f267549d03d5
Julio Cesar <cesar.lopez@t1paginas.com>
2024-07-31 19:20:45 -0600

    fix 3

--- src/app/redux/thunks/Billing.thunk.ts
+++ src/app/redux/thunks/Billing.thunk.ts
@@ -1,6 +1,6 @@
 import BillingService from '@services/BillingService';
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { BillingInfoI } from '@/app/interfaces/Form';
+import { BillingInfoI } from '@Interfaces/Form';
 
 
 export const PostBilling = createAsyncThunk('Billing/PostBilling', async (data: BillingInfoI) => {
